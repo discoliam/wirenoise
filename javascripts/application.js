9305ffad2075ca084e34652c7854b5fb70e9f051
Liam Richardson <discoliam@googlemail.com>
2011-11-24 23:31:07 +0000

    Added js for captions and articles

--- javascripts/application.js
+++ javascripts/application.js
@@ -25,6 +25,61 @@ $(document).ready(function () {
 									]
 
 	});
+	
+	
+	
+  // MENU
+  
+
+  // About
+  $("a.about").click(function() {
+   if( $("article.about").hasClass("open")){
+     $("article.about").animate({left: -250});
+     $("article.about").removeClass("open");
+   } else {
+     $("article.contact").animate({left: -250});
+     $("article.contact").removeClass("open");
+     $("article.about").delay("600").animate({left: 0});
+     $("article.about").addClass("open");
+   } 
+    return false;
+  });
+  
+  // Contact
+  $("a.contact").click(function() {
+   if( $("article.contact").hasClass("open")){
+     $("article.contact").animate({left: -250});
+     $("article.contact").removeClass("open");
+   } else {
+     $("article.about").animate({left: -250});
+     $("article.about").removeClass("open");
+     $("article.contact").delay("600").animate({left: 0});
+     $("article.contact").addClass("open");
+   } 
+    return false;
+  });
+  
+  // Close
+  $("a.close").click(function() {
+   if( $(this).parent('article').hasClass("open")){
+     $(this).parent('article').animate({left: -250});
+     $(this).parent('article').removeClass("open");
+   } 
+    return false;
+  });
+  
+  // Captions
+  $("a.captions").click(function() {
+   if( $("#slidecaption").hasClass("open")){
+     $("#slidecaption").animate({bottom: -42});
+     $("#slidecaption").removeClass("open");
+   } else {
+
+     $("#slidecaption").animate({bottom: 8});
+     $("#slidecaption").addClass("open");
+   } 
+    return false;
+  });
 
 
 }); //eo: Document Ready
