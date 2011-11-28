539f005a637a88d6e0cdccc9824b8ec27c5aa542
Liam Richardson <discoliam@googlemail.com>
2011-11-28 22:52:55 +0000

    fixed up collection changing

--- javascripts/application.js
+++ javascripts/application.js
@@ -3,33 +3,33 @@
 
 // -------------- JQUERY START --------------
 $(document).ready(function () {
-	
-	$.supersized({
-		
-		// Custom
-		progress_bar			: 0, 
-		mousr_scrub			: 0, 
-		autoplay			:0,
-	
-		// Functionality
-		slide_interval          :   3000,		// Length between transitions
-		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		transition_speed		:	700,		// Speed of transition
-												   
-		// Components							
-		slide_links				:	'false',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		slides 					:  	[			// Slideshow Images
-											{image : '/images/photos/joss-look-railride.jpg', title : 'Joss, Look Railride'},
-											{image : '/images/photos/bp-bath-vert-lipslide.jpg', title : 'Ben Pearson, Vert Lipside, Bath'}, 
-											{image : '/images/photos/bp-bath-trails-x-up.jpg', title : 'Ben Pearson, X-Up, Bath Trails'} 
-									]
-
-	});
-	
-	
-	
+  
+  //Load Start
+   $('.images').load('collection1.html');
+  
   // MENU
   
+  // Collection 1
+  $(".collection-1").click(function() {
+    $('#supersized').empty();
+    $('.images').load('collection1.html');
+    return false;
+  });
+  
+  // Collection 2
+  $(".collection-2").click(function() {
+    $('#supersized').empty();
+    $('.images').load('collection2.html');
+    return false;
+  });
+
+
+  // Collection 3
+  $(".collection-3").click(function() {
+    $('#supersized').empty();
+    $('.images').load('collection3.html');
+    return false;
+  });
 
   // About
   $("a.about").click(function() {
